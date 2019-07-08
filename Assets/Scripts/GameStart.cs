6a9408be9c55784b80cc73a078df3c655ded152e
Taoist_Yu <294196463@qq.com>
2019-07-08 17:35:28 +0800

    傻逼东西

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+	void Awake()
+	{
+		//随便调用一个方法激活单例
+		TankModel.Instance.ToString();
+	}
+}
