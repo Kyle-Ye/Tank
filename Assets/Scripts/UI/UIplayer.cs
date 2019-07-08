01f336060119d92c3c451c435baf02e97d0d2ab4
AoqiWang <1017802402@qq.com>
2019-07-08 16:53:34 +0800

    改改改！！！

--- Assets/Scripts/UI/UIplayer.cs
+++ Assets/Scripts/UI/UIplayer.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIplayer : MonoBehaviour
 {
     public int tankId;
     private Tank tankInfo;
+    public Slider bloodSlider, oilSlider;
     // Start is called before the first frame update
     void Start()
     {
